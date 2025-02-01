ff243c378ab906c54a840ad2e7ef2b1384498b9a
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-02-01 15:07:41 +0900

    minilibx-linuxをサブモジュールで追加

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,14 +6,20 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 12:50:14 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/01/26 17:50:09 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/02/01 15:06:51 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "ft_result.h"
+# include "ft_result.h"
+# include "lib/libft/libft.h"
+# include <stddef.h>
+# include <stdbool.h>
+# include "mlx.h"
+
+typedef struct s_game	t_game;
 
 typedef enum e_err_code
 {
@@ -27,6 +33,28 @@ typedef enum e_err_code
 	MAP_003,
 	MAP_004,
 	MAP_005,
-}	t_err_code;
+}						t_err_code;
+
+typedef struct e_vector
+{
+	int					x;
+	int					y;
+}						t_vector;
+
+typedef struct s_game
+{
+	char				**map;
+	int					count;
+	int					coin;
+	t_vector			player;
+	bool				is_clear;
+	void				(*draw)(t_game *self);
+	void				(*update)(t_game *self);
+
+}						t_game;
+
+void					input(t_game *self);
+void					draw(t_game *self);
+void					update(t_game *self);
 
 #endif
